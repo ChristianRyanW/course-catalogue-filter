28155de27cd4d12323920e77b6fb5c105fa96c77
ChristianRyanW <43388784+ChristianRyanW@users.noreply.github.com>
2019-04-13 17:41:58 -0500

    Update SQL_Scriplets.sql
    Added Queries to get all courses with a certain tag and to get all courses that have tags that the user has saved to their account.

--- SQL_Scriplets.sql
+++ SQL_Scriplets.sql
@@ -14,3 +14,13 @@ INSERT INTO SAVES VALUES('MATH', '4400', 'bob@bob.bob');
 DELETE
 FROM SAVES
 WHERE course_subject='MATH' AND course_number='4400' AND user_email='bob@bob.bob';
+
+-- This is to get all courses with a certain tag. Replace the string "AI" with the desired tag value (probably in variable form)
+SELECT course_subject, course_number
+FROM HASQUALITY
+WHERE HASQUALITY.tag_name = 'AI';
+
+-- This is to get all of the courses that have tags that the user has saved.
+SELECT DISTINCT HASQUALITY.course_subject, HASQUALITY.course_number
+FROM HASQUALITY, INTERESTS
+WHERE INTERESTS.user_email='rweeks2010@hotmail.com' AND INTERESTS.tag_name=HASQUALITY.tag_name;
