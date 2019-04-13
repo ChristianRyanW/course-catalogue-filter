3283963f7bb28a88158eb07869d0b685c3ffb49c
ChristianRyanW <43388784+ChristianRyanW@users.noreply.github.com>
2019-04-13 17:13:20 -0500

    Update SQL_Scriplets.sql
    Added SQL statements for adding and removing saved courses from user accounts.

--- SQL_Scriplets.sql
+++ SQL_Scriplets.sql
@@ -4,3 +4,13 @@
 SELECT SAVES.course_subject, SAVES.course_number
 FROM SAVES
 WHERE SAVES.user_email = 'rweeks2010@hotmail.com';
+
+
+-- This is to save a course to a user account. Replace the strings with variables as before
+INSERT INTO SAVES VALUES('MATH', '4400', 'bob@bob.bob');
+
+
+-- This is to delete a course from the user's saved course list. Replace the strings with the variables.
+DELETE
+FROM SAVES
+WHERE course_subject='MATH' AND course_number='4400' AND user_email='bob@bob.bob';
